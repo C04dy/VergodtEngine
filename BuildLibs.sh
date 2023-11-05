891f65de15aa347b956b3bff57556280abb5fb4e
C04dy <yeniv1.00@gmail.com>
2023-11-05 18:32:55 +0300

    Linux library script

--- BuildLibs.sh
+++ BuildLibs.sh
@@ -7,24 +7,41 @@ mkdir SDL
 cd ./SDL
 mkdir include
 mkdir lib
-cd ./include
-mkdir SDL3
-cd ../..
+cd ..
 mkdir Box2D
 cd ./Box2D
 mkdir include
 mkdir lib
-cd ./include
-mkdir box2d
-cd ../../..
+cd ../..
 
 cd ./vendor/SDL
 mkdir build
 cd ./build
-cmake .. -DSDL_STATIC=ON -DSDL_TEST_LIBRARY=OFF
-make
-make install
+sudo cmake .. -DSDL_STATIC=ON -DSDL_TEST_LIBRARY=OFF
+sudo make
+sudo make install
 
 cp -i libSDL3.a ../../../libs/SDL/lib
 cd ../include
-cp -R SDL3 ../../../libs/SDL/include/SDL3
+cp -R SDL3 ../../../libs/SDL/include
+
+cd ../../SDL_image
+mkdir build
+cd ./build
+sudo cmake .. -DBUILD_SHARED_LIBS=OFF
+sudo make
+
+cp -i libSDL3_image.a ../../../libs/SDL/lib
+cd ../include/SDL3_image
+cp -i SDL_image.h ../../../../libs/SDL/include/SDL3
+
+cd ../../../box2d
+mkdir build
+cd ./build 
+sudo cmake .. -DBUILD_SHARED_LIBS=OFF
+sudo make
+
+cd ./bin
+cp -i libbox2d.a ../../../../libs/Box2D/lib
+cd ../../include
+cp -R box2d ../../../libs/Box2D/include/box2d
