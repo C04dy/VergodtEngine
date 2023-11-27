92da6e8080c69d26dffa75ec80178b5656dd8073
C04dy <yeniv1.00@gmail.com>
2023-11-27 19:09:57 +0300

    pybind stuff

--- PybindModule/Module.cpp
+++ PybindModule/Module.cpp
@@ -6,7 +6,7 @@ int add(int i, int j) {
     return i + j;
 }
 
-PYBIND11_MODULE(example, m) {
+PYBIND11_MODULE(test, m) {
     m.doc() = "pybind11 example plugin";
 
     m.def("add", &add, "A function that adds two numbers");
