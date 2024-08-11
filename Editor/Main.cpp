9420eb277b2c35120aea1c88b2c25ee781961a46
C04dy <yeniv1.00@gmail.com>
2024-08-11 20:32:33 +0300

    Started the editor

---
+++ Editor/Main.cpp
@@ -0,0 +1,17 @@
+#include "App.h"
+
+int main()
+{
+    App app;
+
+    try {
+        app.Init();
+        app.Run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+
+}
---
+++ Editor/src/App.h
@@ -0,0 +1,20 @@
+#ifndef APP_H
+#define APP_H
+
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+
+class App
+{
+public:
+    ~App();
+    
+    void Init();
+
+    void Run();
+private:
+};
+
+#endif
